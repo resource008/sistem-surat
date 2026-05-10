5ac06d5c835579b34222feefbcca081f2aa43393
resource008 <aguspranatamarpaung2008@gmail.com>
2026-05-10 10:36:44 +0700

    fix: update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,7 +16,7 @@ export default [
     },
     rules: {
       "no-unused-vars": "off",
-      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-unused-vars": "off"
     }
   }
 ];
