5f25b7e472d685ff22017c65d7745c00b61bd002
resource008 <aguspranatamarpaung2008@gmail.com>
2026-05-10 10:27:54 +0700

    fix: setup ESLint with TypeScript parser

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import js from "@eslint/js";
+import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+
+export default [
+  {
+    ignores: ["node_modules/**", ".next/**", "src/generated/**"]
+  },
+  {
+    files: ["src/**/*.{ts,tsx}"],
+    languageOptions: {
+      parser: tsParser,
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+    },
+    rules: {
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+    }
+  }
+];
